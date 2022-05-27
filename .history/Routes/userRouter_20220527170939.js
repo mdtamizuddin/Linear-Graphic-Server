66f0066eb78f9e048ab9ba4981201f011f1defd0
mdtomiz420 <mdtomiz.official@gmail.com>
2022-05-27 18:06:37 -0400

    server rady

---
+++ .history/Routes/userRouter_20220527170939.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const router = express.Router()
+const userSchema = require('../Schemas/userSchema')
+const User = new mongoose.model('User', userSchema)
+
+router.get('/', (req, res) => {
+    User.find({}, (err, data) => {
+        if (err) {
+            res.status(500).json({ message: "There is A Problem On Server" })
+        }
+        else {
+            res.status(200).json(data)
+        }
+    })
+})
+router.get('/:id', (req, res) => {
+    User.findOne({ '_id': req.params.id }, (err, data) => {
+        if (err) {
+            res.status(500).json({ message: "There is A Problem on Server" })
+        }
+        else {
+            res.status(200).json(data)
+        }
+    })
+})
+
+
+router.put('/:email', (req, res) => {
+    const user = req.body
+    const newUser = new User(req.body)
+    User
+})
+
+router.delete('/:id', (req, res) => {
+    User.deleteOne({ '_id': req.params.id }, (err) => {
+        if (err) {
+            res.status(500).json({ message: "There is A Problem on Server" })
+        }
+        else {
+            res.status(200).json({ message: "data deleted" })
+        }
+    })
+})
+
+module.exports = router
---
+++ .history/index_20220527170815.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 5000
+app.use(bodyParser.json())
+app.use(cors())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcnm4.mongodb.net/linear-graphic?retryWrites=true&w=majority`;
+
+mongoose.connect(uri , {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+})
+
+.then(() => console.log('Database Is Connected'))
+.catch((err)=> console.log(err))
+
+
+//All Routers
+
+app.use('/headers' , require('./Routes/headerRouter'))
+app.use('/portfolio' , require('./Routes/PortfolioRouter'))
+app.use('/review' , require('./Routes/reviewsRouter'))
+app.use('/service' , require('./Routes/ServiceRouter'))
+app.use('/user' , require('./Routes/userRouter'))
+
+
+app.get('/',(req , res)=>{
+    res.send({message : "Server Is Running"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server Is Running On PORT ${PORT}`);
+})
